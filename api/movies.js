1f7e5c9bd8bbba358be6c6bd8cc38d8ea4e4c40c
Kyle Glasper <ksglasper@gmail.com>
2022-08-15 08:35:49 -0500

    api movie functions and had to reinstall npm

--- api/movies.js
+++ api/movies.js
@@ -1,13 +1,97 @@
-const express = require('express')
-const router = express.Router()
+const express = require("express");
+const router = express.Router();
+const { getAllMovies, createMovie, updateMovie, deleteMovie, getMovieById, getANumberOfMoviesBySearchCategory } = require("../db");
 
+router.get("/", async (req, res, next) => {
+  try {
+    const response = await getAllMovies();
 
+    res.send(response);
+  } catch (error) {
+    next({ name: "GET route Error", message: "Error from GET route", error });
+  }
+});
 
+router.post("/", async (req, res, next) => {
+  try {
+    const response = await createMovie(req.body);
+    res.send(response);
+  } catch (error) {
+    next({
+      message: "Error creating movie",
+      name: "CreatingMovieError",
+      error,
+    });
+  }
+});
 
+router.patch("/:movieId", async (req, res, next) => {
+  try {
+    const id  = req.params.movieId;
+    req.body.id = id;
+    if(!(await getMovieById(id))){
+        next({
+            name: "MovieNotFoundError",
+            message: `Movie ${id} not found`,
+            error: "Error!",
+          });
+    }
+    const response = await updateMovie(req.body);
+    res.send(response)
+  } catch (error) {
+    next({ message: "Error updating movie!", name: "UpdateError", error });
+  }
+});
 
+router.delete('/:movieId', async (req, res, next) =>{
+    try {
+        const id  = req.params.movieId
+        if(!(await getMovieById(id))){
+        next({
+            name: "MovieNotFoundError",
+            message: `Movie ${id} not found`,
+            error: "Error!",
+          });
+    }
+    const response = deleteMovie(id)
+    res.send(response)
+} catch (error) {
+    next()
+}
+})
 
+router.get('/:searchMethod/:searchFlow/:limitNumber/:offsetNumber', async(req, res, next) =>{
+    let {searchMethod, searchFlow, limitNumber, offsetNumber} = req.params
+    limitNumber = Number(limitNumber)
+    offsetNumber = Number(offsetNumber)
 
+    try {
+        if(!searchMethod === 'title' || !searchMethod === 'price'){
+            next({
+                searchmessage: 'Not a valid search parameter',
+                error: 'InvalidSearch!'
+            })
+        }
+        if(!searchFlow === 'ASC' || !searchMethod === 'DESC'){
+            next({
+                searchmessage: 'Not a valid search parameter',
+                error: 'InvalidSearch!'
+            })
+        }
 
+        const response = await getANumberOfMoviesBySearchCategory({searchMethod, searchFlow, limitNumber, offsetNumber})
+        res.send(response)
+    } catch (error) {
+        next({message: `Error in searching by method or flow`,
+        name: 'InvalidSearch!',
+        error})
+    }
+})
 
+// get- list of all movies (masterlist)
+// get- list of movies based on different search params
+// post- create movie
+// patch- update movie
+// delete- delete movie
 
-module.exports = router
+module.exports = router;
