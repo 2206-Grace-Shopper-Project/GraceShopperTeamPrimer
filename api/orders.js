28976768a756472d0020d7b44319376479d87740
Dylan Kammerlohr <dylankammerlohr@gmail.com>
2022-08-15 10:19:39 -0500

    finished orders api functions

--- api/orders.js
+++ api/orders.js
@@ -1,13 +1,41 @@
 const express = require('express')
 const router = express.Router()
+const {createOrder, getOrdersByUserId, getAllOrders} = require('../db/models/orders')
 
+// POST create orders
+router.post('/', async (req, res, next) => {
+    const { cartId, address, email, quantity, date, price } = req.body
+    try {
+        const order = await createOrder({ cartId, address, email, quantity, date, price })
+       
+        res.send(order)
+    } catch (error) {
+        console.error("error in create orders")
+        next(error)
+    }
+})
 
+// GET user order history
+router.get('/:userId', async (req, res, next) => {
+    const { userId } = req.params
+    try {
+        const userOrder = await getOrdersByUserId(userId)
+        res.send(userOrder)
+    } catch (error) {
+        console.error("error with getting order by userId")
+        next(error)
+    }
+})
 
-
-
-
-
-
-
+// GET all orders (admin)
+router.get('/', async (req, res, next) => {
+    try {
+        const allUsers = await getAllOrders()
+        res.send(allUsers)
+    } catch (error) {
+        console.error("error getting all orders")
+        next(error)
+    }
+})
 
 module.exports = router
