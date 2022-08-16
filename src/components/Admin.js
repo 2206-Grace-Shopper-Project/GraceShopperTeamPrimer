8dca151337993287cfafde65c767939ce3143d62
Dylan Kammerlohr <dylankammerlohr@gmail.com>
2022-08-16 12:31:35 -0500

    working on orders and admin components

--- src/components/Admin.js
+++ src/components/Admin.js
@@ -1,12 +1,50 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 
 const Admin = () =>{
+    const [orders, setOrders] = useState([])
+
+    const getAllUserOrders = async() => {
+        const ordersList = await getAllOrders(token)
+        setOrders(ordersList)
+    }
+    useEffect(() => {
+        getAllUserOrders()
+    }, [])
+
+
     return(
-        <h1>Hello</h1>
+        <div>
+            <div id="all-orders">
+                {orders.map((order, index) => {
+                    <div key={index}>
+                        <p>{order}</p>
+                    </div>
+                })}
+            </div>
+            <div id="all-users">
+
+            </div>
+        </div>
     )
 }
 
 
 
 export default Admin
+
+// admin function 
+export async function getAllOrders(token) {
+    try {
+        const response = await fetch(`${BASE}/orders`, {
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${token}`,
+            },
+        })
+        const result = await response.json()
+            console.log(result, 'result from getAllOrders')
+    } catch (error) {
+        throw error
+    }
+}
--- src/components/Orders.js
+++ src/components/Orders.js
@@ -1,13 +1,67 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { getUserOrders } from "../api";
 
 
 const Orders = () =>{
+    const [orders, setOrder] = useState([])
+
+    const getUserOrderInfo = async () => {
+        const user = await getUser(id, token)
+            console.log(user, 'user in getUserOrderInfo')
+        const userOrders = await getUserOrders(userId, token)
+            console.log(userOrders, 'userOrders')
+        setOrder(userOrders)
+    }
+    useEffect(() => {
+        getUserOrderInfo()
+    }, [])
+
     return(
-        <h1>Hello</h1>
+        <div>
+            <h1>Orders</h1>
+            {orders.map((order, index) => {
+                <div key={index}>
+                    <p>{new Date(order.date)}</p>
+                    <p>{order.quantity}</p>
+                    <p>{order.price}</p>
+                    <p>{order.address}</p>
+                </div>
+            })}
+        </div>
     )
 }
 
 
 
 export default Orders
+
+
+
+export async function getUserOrders(userId, token) {
+    try {
+        const response = await fetch(`${BASE}/orders/${userId}`, {
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${token}`,
+            },
+        })
+        const result = await response.json()
+            console.log(result, 'result from getUserOrders')
+        return result
+    } catch (error) {
+        throw error
+    }
+}
+
+
+export const getUser = async(id, token) => {
+    const response = await fetch(`${BASE}/users/${id}`, {
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${token}`
+        }, 
+    })
+    const result = await response.json()
+        console.log(result, 'result from getUser')
+    return result
+}
