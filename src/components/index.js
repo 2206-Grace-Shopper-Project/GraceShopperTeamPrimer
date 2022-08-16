5ff03845a49d51a0e681041435bb61c0660481d6
Hunter Norris <hunter.t.norris@gmail.com>
2022-08-16 12:31:31 -0500

    middle of fixing the login function

--- src/components/index.js
+++ src/components/index.js
@@ -5,6 +5,9 @@ export { default as Movies } from './Movies';
 export { default as Carts } from './Carts';
 export { default as Orders } from './Orders';
 export { default as Header } from './Header';
+export { default as Register } from './Register';
+export { default as Login } from './Login';
+
 
 
 
