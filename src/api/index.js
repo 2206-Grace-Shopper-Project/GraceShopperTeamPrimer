ebb2fc1381818b41184562c90c146dd7cf40baee
Darius Robinson <robinson.darius090@gmail.com>
2022-08-16 13:31:47 -0400

    working on cart components

--- src/api/index.js
+++ src/api/index.js
@@ -5,25 +5,53 @@ export const BASE = `https://radiant-citadel-20620.herokuapp.com/api`;
 // MOVIE FUNCTIONS
 
 // CART FUNCTIONS
+export const createNewCart = async (userId) => {
+  try {
+    const response = await fetch(`${BASE}/carts`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        userId,
+      }),
+    });
+    const result = await response.json();
+    console.log(result, "this is result from api create cart");
+    return result;
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+
+export const getEachCartByUser = async (userId) => {
+try {
+    const response = await fetch(`${BASE}/carts/${userId}`, {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+      const result = await response.json();
+      return result;
+} catch (error) {
+    console.error(error)
+}
+}
 
 // ORDERS FUNCTIONS
 export async function getUserOrders(userId) {
-    try {
-        const response = await fetch(`${BASE}/orders/${userId}`, {
-            headers: {
-                'Content-Type': 'application/json',
-                'Authorization': `Bearer ${token}`,
-            },
-        })
-        console.log(token, 'token')
-        const result = await response.json()
-        return result
-    } catch (error) {
-        
-    }
-
+  try {
+    const response = await fetch(`${BASE}/orders/${userId}`, {
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+    });
+    console.log(token, "token");
+    const result = await response.json();
+    return result;
+  } catch (error) {}
 }
 
-export async function getAllOrders() {
-    
-}
+export async function getAllOrders() {}
--- src/components/Carts.js
+++ src/components/Carts.js
@@ -1,9 +1,29 @@
 import React from "react";
+import { createNewCart } from "../api";
+
+const Carts = (userId) =>{
 
 
-const Carts = () =>{
+    //onClick for create a new cart 
+const handleOnClick = async (event) => {
+    event.preventDefault();
+    const response = await createNewCart(userId)
+    return console.log(response, "this is the respnse from the Cart component")
+
+}
+
+
+
     return(
-        <h1>Hello</h1>
+        <>
+        <button onClick={handleOnClick}>Create Cart</button>
+        <>
+        {/* <button onClick={async (event)=>{
+            event.preventDefault();
+            const 
+        }}></button> */}
+        </>
+        </>
     )
 }
 
