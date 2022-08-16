954acb31b55e85488900e8d5ed0d9f5e5477ca43
Kyle Glasper <ksglasper@gmail.com>
2022-08-16 12:31:42 -0500

    header component work and movie component work

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -1,5 +1,22 @@
 import React from "react";
+export const BASE = `https://radiant-citadel-20620.herokuapp.com/api`;
 
+export async function getUserOrders(userId) {
+    try {
+        const response = await fetch(`${BASE}/orders/${userId}`, {
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${token}`,
+            },
+        })
+        console.log(token, 'token')
+        const result = await response.json()
+        return result
+    } catch (error) {
+        
+    }
+
+}
 
 const Movies = () =>{
     return(
