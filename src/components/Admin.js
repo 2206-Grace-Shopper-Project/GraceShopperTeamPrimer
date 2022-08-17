f2d8b0ac7582d21d68dadbe33fdb9471d6f1d522
Dylan Kammerlohr <dylankammerlohr@gmail.com>
2022-08-17 10:08:22 -0500

    admin and orders components

--- src/components/Admin.js
+++ src/components/Admin.js
@@ -1,22 +1,35 @@
 import React, { useState, useEffect } from "react";
-import { getAllOrders } from "../api";
+// import { getAllOrders } from "../api";
 
 export const BASE = `https://radiant-citadel-20620.herokuapp.com/api`;
-export async function getAllUsers(token){
+export async function getAllUsers(){
     try {
         const response = await fetch(`${BASE}/users`, {
             headers: {
-                "Content-Type": "application/json",
-                "Authorization": `Bearer ${token}`,
+                "Content-Type": "application/json"
               },
         })
-        const result = response.json()
+        const result = await response.json()
         console.log(result, 'result from getAllUsers')
         return result
     } catch(error){
         throw error
     }
 }
+
+// export async function getAllOrders() {
+//     try {
+//       const response = await fetch(`${BASE}/orders`, {
+//         headers: {
+//           "Content-Type": "application/json"
+//         },
+//       });
+//       const result = await response.json();
+//       console.log(result, "result from getAllOrders");
+//     } catch (error) {
+//       throw error;
+//     }
+//   }
 
 
 const Admin = ({token}) =>{
@@ -27,11 +40,12 @@ const Admin = ({token}) =>{
         const usersList = await getAllUsers()
         setUsers(usersList)
     }
-
     const getAllUserOrders = async() => {
-        const ordersList = await getAllOrders(token)
+        const ordersList = await getAllOrders()
+        console.log(ordersList, 'here')
         setOrders(ordersList)
     }
+    console.log(orders)
     useEffect(() => {
         getAllUserOrders();
         getUsers();
@@ -43,18 +57,25 @@ const Admin = ({token}) =>{
             <div id="all-orders">
                 <h1>All Orders</h1>
                 {/* {orders.map((order, index) => {
-                    <div key={index}>
-                        <p>{order}</p>
-                    </div>
+                    console.log(order,'line 47')
+                    return (
+                        <div key={index}>
+                            <p>{order}</p>
+                        </div>
+                    )
                 })} */}
             </div>
             <div id="all-users">
                 <h1>All Users</h1>
-                {/* {users.map((user, index)=> {
-                    <div key={index}>
-                        <p>{user}</p>
-                    </div>
-                })} */}
+                {users.map((user, index)=> {
+                    return (
+                        <div key={index}>
+                            <p>{user.id}</p>
+                            <p>{user.name}</p>
+                            <p>{user.email}</p>
+                        </div>
+                    )
+                })}
             </div>
         </div>
     )
