8b862316cc43f8013ec18e92410d0dda5e1bbb0e
Kyle Glasper <ksglasper@gmail.com>
2022-08-17 08:39:14 -0500

    movie layout setup in component

--- src/components/App.js
+++ src/components/App.js
@@ -3,8 +3,37 @@ import {  Routes, Route } from 'react-router-dom';
 import { grabToken } from '../auth';
 import {Orders, UserForm, Movies, Carts, Header, Register, Login} from './'
 
+
+export const BASE = `https://radiant-citadel-20620.herokuapp.com/api`;
+
+export async function getAllMovies() {
+    try {
+        const response = await fetch(`${BASE}/movies`, {
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        })
+        const result = await response.json()
+        console.log(result, '!!!!!!!!')
+        return result
+    } catch (error) {
+        
+    }
+
+}
+
 const App = () => {
   const [token, setToken] = useState(grabToken()) 
+  const [allMovies, setAllMovies] = useState([])
+
+  const fetchMovies = async ()=>{
+    const movieList = await getAllMovies()
+    setAllMovies(movieList)
+}
+useEffect(()=>{
+   fetchMovies()
+
+}, [])
 
   return (
     <>
@@ -12,7 +41,7 @@ const App = () => {
       <Route path='/' />
       <Route path='/orders' element={<Orders setToken={setToken} token={token}/>}/>
       <Route path='/users' element={<UserForm setToken={setToken} token={token}/>}/>
-      <Route path='/movies' element={<Movies setToken={setToken} token={token}/>}/>
+      <Route path='/movies' element={<Movies setToken={setToken} token={token} allMovies={allMovies}/>}/>
       <Route path='/carts' element={<Carts setToken={setToken} token={token}/>}/>
       <Route path='/header' element={<Header setToken={setToken} token={token}/>}/>
       <Route path='/login' element={<Login setToken={setToken} token={token}/>}/>
--- src/components/Movies.js
+++ src/components/Movies.js
@@ -1,26 +1,100 @@
-import React from "react";
-export const BASE = `https://radiant-citadel-20620.herokuapp.com/api`;
+import React, { useEffect, useState } from "react";
+import { NavLink} from "react-router-dom";
+// export const BASE = `https://radiant-citadel-20620.herokuapp.com/api`;
 
-export async function getUserOrders(userId) {
-    try {
-        const response = await fetch(`${BASE}/orders/${userId}`, {
-            headers: {
-                'Content-Type': 'application/json',
-                'Authorization': `Bearer ${token}`,
-            },
-        })
-        console.log(token, 'token')
-        const result = await response.json()
-        return result
-    } catch (error) {
+// export async function getAllMovies() {
+//     try {
+//         const response = await fetch(`${BASE}/movies`, {
+//             headers: {
+//                 'Content-Type': 'application/json',
+//             },
+//         })
+//         const result = await response.json()
+//         console.log(result, '!!!!!!!!')
+//         return result
+//     } catch (error) {
         
-    }
+//     }
+
+// }
+
+const Movies = ({allMovies}) =>{
+    // const [allMovies, setAllMovies] = useState([])
+    const [cssActive, setCSSActive] = useState(null)
+    const [activeCart, setActiveCart] = useState(null)
+    // const fetchMovies = async ()=>{
+    //     const movieList = await getAllMovies()
+    //     setAllMovies(movieList)
+    // }
+    // useEffect(()=>{
+    //    fetchMovies()
+    
+    // }, [])
+// console.log(allMovies, '###')
+
+const handleOnclick = (event) =>{
+    event.preventDefault()
+    console.log(event.target)
 
 }
-
-const Movies = () =>{
     return(
-        <h1>Hello</h1>
+        <>
+        <h1 id="movieHeader">Welcome, Find a Movie!</h1>
+        <div id="movieComponent">
+
+        {allMovies && allMovies.length ? allMovies.map((movie, index)=>{
+            let title = movie.title
+            let genre = movie.genre
+            let year = movie.year
+            let plot = movie.plot
+            let price = movie.price
+            let id = movie.id
+            let inventory = movie.inventory
+            let className = null
+            if(cssActive === movie.id){
+                className = 'activeSeeMore'
+            }
+
+            return(
+            <div key={id} className="movieContainer" >
+            <img className="moviePoster" src={movie.poster}/>
+              <div className="textContainer">
+                <div className="priceText movieText">
+                    <span>${price}.99</span>
+                    <br></br>
+                    {inventory < 10 && inventory > 0  ? <><span className="almostOutOfStock">Only {inventory} left in stock</span><br></br></> : <br></br>}
+                    {inventory === 0 ? <><span className="outOfStock">Out of stock</span><br></br></> : <br></br>}
+                    <button value={index} className="addToCart" onClick={handleOnclick}><span>Add to Cart</span></button>
+                    </div>
+              </div>
+              <div className="movieInfoContainer"> <NavLink style={{color: 'black'}} to={`/movies/${title}`}>{title}  ({year})</NavLink>
+              <div className={`${className} movieInfo`}>{genre}  
+              <br></br> 
+              <br></br> 
+              <span className='furtherMovieInfo'>{plot} </span>
+              <br></br> 
+              <br></br> 
+              <span className='furtherMovieInfo'>Starring: {movie.actors} </span>
+              <br></br>
+              <br></br> 
+              <span className='furtherMovieInfo'>Directors: {movie.directors} </span>
+              <br></br> 
+              <span className='furtherMovieInfo'>{movie.rated} </span>
+              </div>
+              <p className="seeMoreTag"><a className="seeMoreLink" href="#" onClick={
+                (event)=>{
+                event.preventDefault()
+                cssActive && cssActive === movie.id ? setCSSActive(null) : setCSSActive(movie.id) 
+                console.log(cssActive)}}>
+                    { cssActive === movie.id ? '↑ See less' : '↓ See more' }</a>
+                </p>
+              </div>
+              
+            </div>
+            )
+        }) : <>Something Broke</>}
+        </div>
+        </>
     )
 }
 
