1d5c3255d4b002697974dadf2876d20edb6cd5a8
Darius Robinson <robinson.darius090@gmail.com>
2022-08-19 11:20:35 -0400

    worked on remove movie component

--- src/components/RemoveMovie.js
+++ src/components/RemoveMovie.js
@@ -1,29 +1,20 @@
 import React, { useState } from "react";
 import { removeMovieFromACart, getEachCartByUser } from "../api";
 
-const RemoveMovie = ({userDataObj}) => {
+const RemoveMovie = ({userDataObj, CMI}) => {
     const [movies, setMovies] = useState([])
     const userId = userDataObj.id
-console.log(userDataObj,'UDO')
-console.log(userId)
+    let id = CMI
 
 
     async function handleOnClick (event) {
         event.preventDefault();
+       
         const userCart = await getEachCartByUser(userId) 
         console.log(userCart) 
+        const response = await removeMovieFromACart(id)
         setMovies(userCart)
-        movies.map((item)=>{
-             let movie = item.movies
-             return (
-                movie.map(async (movies)=>{
-                    let id = movies.cartMoviesId
-                    const response = await removeMovieFromACart(id)
-                    console.log(response, "this is response from Map")
-                })
-             )
-        })
-        console.log(movies, 'this is movies array')
+    console.log(movies,'this is movies')
 
        
     }
