c3d51fbf34a1cc43e66fa3ab537ca9a898fbcd70
Darius Robinson <robinson.darius090@gmail.com>
2022-08-20 16:21:24 -0400

    working on udating the cart  page without refreshing

--- src/components/RemoveMovie.js
+++ src/components/RemoveMovie.js
@@ -1,27 +1,40 @@
 import React, { useState } from "react";
 import { removeMovieFromACart, getEachCartByUser } from "../api";
 
-const RemoveMovie = ({userDataObj, CMI}) => {
-    const [movies, setMovies] = useState([])
-    const userId = userDataObj.id
-    let id = CMI
+const RemoveMovie = ({ userDataObj, CMI, myCart, setMyCart }) => {
+  const [movies, setMovies] = useState([]);
+  const userId = userDataObj.id;
+  let id = CMI;
 
+  async function handleOnClick(event) {
+    event.preventDefault();
 
-    async function handleOnClick (event) {
-        event.preventDefault();
-        const userCart = await getEachCartByUser(userId) 
-        const response = await removeMovieFromACart(id)
-        setMovies(userCart)
-    console.log(response,'this is movies') 
+    console.log(myCart, "this is MYCART!!!!");
+    if (myCart) {
+      const userCart = await getEachCartByUser(userId);
+      await removeMovieFromACart(id);
+      
+            console.log(myCart, "MMOVIESSSSSSSS")
+      let deletedMovie = [...myCart.movies];
+      deletedMovie.forEach((element, index) => {
+        if (element.id === CMI) {
+            console.log("element Id should be CMI",element.id)
+        //   deletedMovie.splice(index, 0);
+        }
+      });
+      console.log(deletedMovie,"this shoud not include the delted movie")
     }
 
+    //     // const response = await removeMovieFromACart(id)
+    //     // setMovies(userCart)
+    // console.log(response,'this is movies')
+  }
 
-    return(
-        <>
-        <button onClick={handleOnClick}>Remove Movie</button>
-        </>
-    )
-}
+  return (
+    <>
+      <button onClick={handleOnClick}>Remove Movie</button>
+    </>
+  );
+};
 
-
-export default RemoveMovie
+export default RemoveMovie;
