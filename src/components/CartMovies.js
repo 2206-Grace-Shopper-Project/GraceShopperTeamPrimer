2c8683a86e5c12ba29bf3ba78f6855af75430dfe
Darius Robinson <robinson.darius090@gmail.com>
2022-08-21 18:06:28 -0400

    changed the edit to only show for one movie at a time

--- src/components/CartMovies.js
+++ src/components/CartMovies.js
@@ -1,29 +1,32 @@
-import React, { useState } from "react";
-import {
-  addMovieToCart,
-  removeMovieFromACart,
-  updateMovieQuantity,
-  getCartMoviesById,
-  getEachCartByUser,
-} from "../api";
+import React from "react";
+import { addMovieToCart, getEachCartByUser } from "../api";
 
-const CartMovies = ({ userDataObj, purchaseAmount, id, realPrice, title, showButton }) => {
-  
+const CartMovies = ({
+  userDataObj,
+  purchaseAmount,
+  id,
+  realPrice,
+  title,
+  showButton,
+}) => {
+  let userId = null;
 
-
-  let userId = null
-
-  if(userDataObj){
-    userId= userDataObj.id
+  if (userDataObj) {
+    userId = userDataObj.id;
   }
-
 
   const handleOnClick = async (event) => {
     event.preventDefault();
     const currentCart = await getEachCartByUser(userId);
-   
+
+    //search if !userDataObj
+    //if true the create guest user name = guest pw=guestuser
+    //api call
+    //save in local storage for guest
+    //clear local storage at checkout
+
     if (purchaseAmount === 0) {
-      let quantity = 1 
+      let quantity = 1;
       const cartId = currentCart.id;
       const movieId = id;
       const response = await addMovieToCart(cartId, movieId, quantity);
@@ -39,20 +42,24 @@ const CartMovies = ({ userDataObj, purchaseAmount, id, realPrice, title, showBut
 
   return (
     <>
-     
-      {showButton ? <button
-        id = "add-to-cart"
-        // className="priceText textContainer topRowContainer movieContainer"
-        onClick={handleOnClick}
-      >
-       Add To Cart
-      </button> :  <button
-        className="addToCart priceText textContainer topRowContainer movieContainer"
-        onClick={handleOnClick}
-      >
-        <span class="material-symbols-outlined" id='add-cart-icon'>add_shopping_cart</span>
-      </button>}
-      
+      {showButton ? (
+        <button
+          id="add-to-cart"
+          // className="priceText textContainer topRowContainer movieContainer"
+          onClick={handleOnClick}
+        >
+          Add To Cart
+        </button>
+      ) : (
+        <button
+          className="addToCart priceText textContainer topRowContainer movieContainer"
+          onClick={handleOnClick}
+        >
+          <span class="material-symbols-outlined" id="add-cart-icon">
+            add_shopping_cart
+          </span>
+        </button>
+      )}
     </>
   );
 };
