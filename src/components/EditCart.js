4bcd43847d3549a25f9f479e3b127dc8bd5d0c69
Darius Robinson <robinson.darius090@gmail.com>
2022-08-21 14:46:21 -0400

    set the edit bar to close when submit

--- src/components/EditCart.js
+++ src/components/EditCart.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { getEachCartByUser, updateMovieQuantity, addMovieToCart, } from "../api";
+import { getEachCartByUser, updateMovieQuantity } from "../api";
 
 const EditCart = ({
   userDataObj,
@@ -17,59 +17,44 @@ const EditCart = ({
     console.log("userId in Edit cart", userId);
     console.log("CMI", id);
 
-// going to bring in a addMovieToCart function
-//  send the response to replace that movie and add it to a copy of an array and then try and splice the movies array in my cart to update the mycart object in whole and get it to automatically update
+    // going to bring in a addMovieToCart function
+    //  send the response to replace that movie and add it to a copy of an array and then try and splice the movies array in my cart to update the mycart object in whole and get it to automatically update
 
+    if (myCart) {
+      let quantity = event.target.quantity.value;
+      const cart = await getEachCartByUser(userId);
+      console.log(cart, "just chcking my cart for today");
+      const newQ = await updateMovieQuantity(id, quantity);
+      console.log(newQ, "newQuantity");
+      let cartId = newQ.cartId;
+      let movieId = newQ.movieId;
+      quantity = newQ.quantity;
+      console.log(quantity, "QUANTITY");
+      console.log(movieId, "MOVIEID");
+      console.log(cartId, "CARTID");
 
+      let newMovies = [...myCart.movies];
+      newMovies.map((element) => {
+        if (
+          movieId === element.id &&
+          cartId === element.cartId &&
+          CMI === element.cartMoviesId
+        ) {
+          element.quantity = quantity;
+        }
+        console.log(element.quantity, "this should be the new quantity");
+        let finalMovies = [...newMovies];
+        console.log(finalMovies, "is this possible");
+        cart.movies = finalMovies;
+      });
 
-if(myCart){
-  let quantity = event.target.quantity.value;
-const cart = await getEachCartByUser(userId)
-console.log(cart, 'just chcking my cart for today')
-const  newQ = await updateMovieQuantity(id, quantity)
-console.log(newQ,'newQuantity')
-let cartId = newQ.cartId
-let movieId = newQ.movieId
-quantity = newQ.quantity
-console.log(quantity,"QUANTITY")
-console.log(movieId,"MOVIEID")
-console.log(cartId, "CARTID")
+      setMyCart(cart);
+      setCanEdit(false);
 
+      // let newMovieObj = await addMovieToCart(cartId, movieId, quantity)
 
-let newMovies = [...myCart.movies]
- newMovies.map((element)=>{
-      if(movieId === element.id && cartId === element.cartId && CMI === element.cartMoviesId ){
-          element.quantity = quantity
-      }
-      console.log(element.quantity,"this should be the new quantity")
-      let finalMovies = [...newMovies]
-      console.log(finalMovies,"is this possible")
-      cart.movies = finalMovies
-      
- })
-
-setMyCart(cart)
-
-
-
-
-
-// let newMovieObj = await addMovieToCart(cartId, movieId, quantity)
-
-// console.log(newMovieObj,"this is a new movie")
-
-
-}
-
-
-
-
-
-
-
-
-
-
+      // console.log(newMovieObj,"this is a new movie")
+    }
 
     // if (myCart) {
     //   const cart = await getEachCartByUser(userId);
@@ -80,19 +65,19 @@ setMyCart(cart)
     //   console.log("this should be your cart", cart);
     //   const response = await updateMovieQuantity(id, quantity);
     //   console.log("this is response from edit movie", response);
-      
+
     //   const newQuantity = [...myCart.movies];
     //   newQuantity.forEach((element, index) => {
     //       if (element.cartMoviesId === id) {
     //          console.log(element, 'this is your element')
     //         if(element.quantity){
     //             response.quantity = element.quantity
-                
+
     //           newQuantity.splice(index, 1, response);
     //           cart.movies[10] = newQuantity;}
-           
+
     //         }
-            
+
     //         setMyCart(cart);
     //         setCanEdit(false);
     //   });
